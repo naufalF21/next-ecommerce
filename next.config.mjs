7048087c3d5cdfe95862eec4d52a76afd746036d
naufalF21 <naufalfirman098@gmail.com>
2025-01-28 20:13:06 +0700

    feat : add Navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: { formats: ['image/avif', 'image/webp'], domains: ['images.pexels.com'] },
+};
 
 export default nextConfig;
