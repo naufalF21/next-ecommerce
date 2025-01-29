f5809212393679c98c77cf3cd5e6498adbd6a47f
naufalF21 <naufalfirman098@gmail.com>
2025-01-29 16:11:55 +0700

    feat: Featured Product

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,115 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const ProductList = () => {
+	return (
+		<div className="flex gap-x-8 gap-y-16 justify-between flex-wrap mt-12">
+			<Link href="/" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+				<div className="relative w-full h-80">
+					<Image
+						src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in duration-500"
+					/>
+					<Image
+						src="https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md"
+					/>
+				</div>
+				<div className="flex justify-between">
+					<span className="font-medium">Product Name</span>
+					<span className="font-semibold">$49</span>
+				</div>
+				<div className="text-sm text-gray-500">My Description</div>
+				<button className="rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white w-max">
+					Add to Cart
+				</button>
+			</Link>
+			<Link href="/" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+				<div className="relative w-full h-80">
+					<Image
+						src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in duration-500"
+					/>
+					<Image
+						src="https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md"
+					/>
+				</div>
+				<div className="flex justify-between">
+					<span className="font-medium">Product Name</span>
+					<span className="font-semibold">$49</span>
+				</div>
+				<div className="text-sm text-gray-500">My Description</div>
+				<button className="rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white w-max">
+					Add to Cart
+				</button>
+			</Link>
+			<Link href="/" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+				<div className="relative w-full h-80">
+					<Image
+						src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in duration-500"
+					/>
+					<Image
+						src="https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md"
+					/>
+				</div>
+				<div className="flex justify-between">
+					<span className="font-medium">Product Name</span>
+					<span className="font-semibold">$49</span>
+				</div>
+				<div className="text-sm text-gray-500">My Description</div>
+				<button className="rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white w-max">
+					Add to Cart
+				</button>
+			</Link>
+			<Link href="/" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+				<div className="relative w-full h-80">
+					<Image
+						src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in duration-500"
+					/>
+					<Image
+						src="https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&lazy=load"
+						alt=""
+						fill
+						sizes="25vw"
+						className="absolute object-cover rounded-md"
+					/>
+				</div>
+				<div className="flex justify-between">
+					<span className="font-medium">Product Name</span>
+					<span className="font-semibold">$49</span>
+				</div>
+				<div className="text-sm text-gray-500">My Description</div>
+				<button className="rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white w-max">
+					Add to Cart
+				</button>
+			</Link>
+		</div>
+	);
+};
+
+export default ProductList;
